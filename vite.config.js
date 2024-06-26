2e0b22b1268e15e51b16ca8a71f9ea6e7b21f658
Gaétan Zwick <gaetan.zwick.dev@gmail.com>
2024-06-26 11:53:36 +0200

    init basic three.js program

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import restart from "vite-plugin-restart";
+
+export default {
+  root: "src/",
+  publicDir: "../static/",
+  server: {
+    host: true,
+    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: "../dist", // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+  },
+  plugins: [
+    restart({ restart: ["../static/**"] }), // Restart server on static file change
+  ],
+};
