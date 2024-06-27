a386cbca4755f72a5d7843d6fc4ea37df76d61fd
Gaétan Zwick <gaetan.zwick.dev@gmail.com>
2024-06-27 11:12:41 +0200

    added HistoryItem (2)

--- src/Experience/WaveFunctionCollapse/HistoryItem.js
+++ src/Experience/WaveFunctionCollapse/HistoryItem.js
@@ -1,6 +1,13 @@
 class HistoryItem {
   constructor(slot) {
+    /**
+     * @type {Map<THREE.Vector3, ModuleSet>}
+     */
     this.removedModules = new Map();
+
+    /**
+     * @type {Slot}
+     */
     this.slot = slot;
   }
 }
